ad89fb24f0739cb9108526995b25048ec1df62dc
이인범 <inbeom@dhcp-vl2041-34293.eduroam.cornell.edu>
2021-10-29 13:47:24 -0400

    Updated AsyVar

--- R/AsyVar.R
+++ R/AsyVar.R
@@ -15,53 +15,56 @@
 #' @export
 #'
 #' @examples #GENERATING THE DATA
-#'           n=300
+#'n=300
 #'           
-#'           #Initialize the X matrix.
-#'           X_1 <- rnorm(n,3,sqrt(2))
-#'           X_2 <- rnorm(n,0,1)
-#'           X_3 <- rnorm(n,0,1)
-#'           X_4 <- rnorm(n,0,1)
-#'           X_mat <- as.matrix(cbind(rep(1,n), X_1, X_2, X_3, X_4)) 
+#'#Initialize the X matrix.
+#'X_1 <- rnorm(n,3,sqrt(2))
+#'X_2 <- rnorm(n,0,1)
+#'X_3 <- rnorm(n,0,1)
+#'X_4 <- rnorm(n,0,1)
+#'X_mat <- as.matrix(cbind(rep(1,n), X_1, X_2, X_3, X_4)) 
 #'           
-#'           #Initialize the Y_1 and Y_0 vector using the treatment model and the control model.
-#'           Y_1 <- X_mat %*% matrix(c(200, 27.4, 13.7, 13.7, 13.7), 5, 1) + rnorm(n)
-#'           Y_0 <- X_mat %*% matrix(c(200, 0 , 13.7, 13.7, 13.7), 5, 1) + rnorm(n)
+#'#Initialize the Y_1 and Y_0 vector using the treatment model and the control model.
+#'Y_1 <- X_mat %*% matrix(c(200, 27.4, 13.7, 13.7, 13.7), 5, 1) + rnorm(n)
+#'Y_0 <- X_mat %*% matrix(c(200, 0 , 13.7, 13.7, 13.7), 5, 1) + rnorm(n)
 #'           
-#'           #True Propensity Score calculation.
-#'           pre_prop <- X_mat[,2:5] %*% matrix(c(-beta, 0.5, -0.25, -0.1), 4, 1)
-#'           propensity_true <- (exp(pre_prop))/(1+(exp(pre_prop)))
+#'#True Propensity Score calculation.
+#'pre_prop <- X_mat[,2:5] %*% matrix(c(0, 0.5, -0.25, -0.1), 4, 1)
+#'propensity_true <- (exp(pre_prop))/(1+(exp(pre_prop)))
 #'           
-#'           #Generate T_vec, the treatment vector, with the true propensity scores.
-#'           T_vec <- rbinom(n, size=1, prob=propensity_true)
+#'#Generate T_vec, the treatment vector, with the true propensity scores.
+#'T_vec <- rbinom(n, size=1, prob=propensity_true)
 #'           
-#'           #Now generate the actual outcome Y_outcome (accounting for treatment/control groups).
-#'           Y_outcome <- Y_1*T_vec + Y_0*(1-T_vec)
+#'#Now generate the actual outcome Y_outcome (accounting for treatment/control groups).
+#'Y_outcome <- Y_1*T_vec + Y_0*(1-T_vec)
 #'           
-#'           #NOW ACTUALLY STARTING THE ESTIMATION
-#'           #First, fit the linear regression model for T_vec=1 and T_vec=0 separately.
-#'           Y_outcome_1 <- Y_outcome[which(T_vec==1)]
-#'           Y_outcome_0 <- Y_outcome[which(T_vec==0)]
+#'#NOW ACTUALLY STARTING THE ESTIMATION
+#'#First, fit the linear regression model for T_vec=1 and T_vec=0 separately.
+#'Y_outcome_1 <- Y_outcome[which(T_vec==1)]
+#'Y_outcome_0 <- Y_outcome[which(T_vec==0)]
 #'           
-#'           #Separate the treatment covariates and the control covariates. 
-#'           X_mat_1 <- X_mat[which(T_vec==1),c(2,3,4,5)]
-#'           X_mat_0 <- X_mat[which(T_vec==0),c(2,3,4,5)]
+#'#Separate the treatment covariates and the control covariates. 
+#'X_mat_1 <- X_mat[which(T_vec==1),c(2,3,4,5)]
+#'X_mat_0 <- X_mat[which(T_vec==0),c(2,3,4,5)]
 #'           
-#'           #Perform linear regression separately.
-#'           lin_1 <- lm(Y_outcome_1 ~ X_mat_1) 
-#'           lin_0 <- lm(Y_outcome_0 ~ X_mat_0)
+#'#Perform linear regression separately.
+#'lin_1 <- lm(Y_outcome_1 ~ X_mat_1) 
+#'lin_0 <- lm(Y_outcome_0 ~ X_mat_0)
 #'           
-#'           #Y_hat(1|X_i), #Y_hat(0|X_i) values 
-#'           Y_1_hat <- X_mat%*%as.matrix(lin_1$coefficients,5,1)  
-#'           Y_0_hat <- X_mat%*%as.matrix(lin_0$coefficients,5,1) 
+#'#Y_hat(1|X_i), #Y_hat(0|X_i) values 
+#'Y_1_hat <- X_mat%*%as.matrix(lin_1$coefficients,5,1)  
+#'Y_0_hat <- X_mat%*%as.matrix(lin_0$coefficients,5,1) 
 #'           
-#'           #Use oCBPS to get estimated propensity scores pi and the estimated average treatment effect mu_hat.
-#'           ocbps.fit <- CBPS(T_vec ~ X_mat, ATT=0, baseline.formula = ~X_mat[,c(1,3:5)], diff.formula = ~X_mat[,2])
-#'           pi_hat <- ocbps.fit$fitted.values 
-#'           mu_hat <- mean(((T_vec)*Y_outcome/pi_hat) - (((T_vec)*Y_outcome)/(1-pi_hat)))
+#'#Use oCBPS to get estimated propensity scores pi and the estimated average treatment effect mu_hat.
+#'ocbps.fit <- CBPS(T_vec ~ X_mat, ATT=0, baseline.formula = ~X_mat[,c(1,3:5)], 
+#'                  diff.formula = ~X_mat[,2])
+#'pi_hat <- ocbps.fit$fitted.values 
+#'mu_hat <- mean(((T_vec)*Y_outcome/pi_hat) - (((T_vec)*Y_outcome)/(1-pi_hat)))
 #'           
-#'           #Use the AsyVar function to get the asymptotic variance of the estimated average treatment effect and its confidence interval
-#'           AsyVar(Y=Y_outcome, Y_1_hat=Y_1_hat, Y_0_hat=Y_0_hat, CBPS_obj=ocbps.fit, X=X_mat, TL=T_vec, pi=pi_hat, mu=mu_hat, CI=0.95)
+#'#Use the AsyVar function to get the asymptotic variance of the 
+#'#estimated average treatment effect and its confidence interval
+#'AsyVar(Y=Y_outcome, Y_1_hat=Y_1_hat, Y_0_hat=Y_0_hat, CBPS_obj=ocbps.fit, 
+#'       X=X_mat, TL=T_vec, pi=pi_hat, mu=mu_hat, CI=0.95)
 #'           
 AsyVar <- function(Y, Y_1_hat, Y_0_hat, CBPS_obj=NULL, X, TL, pi, mu, CI=0.95){
   
@@ -99,7 +102,7 @@ AsyVar <- function(Y, Y_1_hat, Y_0_hat, CBPS_obj=NULL, X, TL, pi, mu, CI=0.95){
     
     result[[2]] <- result[[1]]/n
     
-    diff_ocbps <- qnorm(1-(1-CI)/2)*sqrt(result[[1]]/n)
+    diff_ocbps <- stats::qnorm(1-(1-CI)/2)*sqrt(result[[1]]/n)
     lower_ocbps <- mu_hat - diff_ocbps
     upper_ocbps <- mu_hat + diff_ocbps  
     
